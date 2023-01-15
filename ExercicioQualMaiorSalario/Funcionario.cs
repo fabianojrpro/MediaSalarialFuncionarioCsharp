4f4e77c3a0093656984c5b452a93517ad0ba8c73
Fabiano Junior <99301175+fabianojrpro@users.noreply.github.com>
2023-01-15 20:19:16 -0300

    Add files via upload

---
+++ ExercicioQualMaiorSalario/Funcionario.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioQualMaiorSalario
+{
+    internal class Funcionario
+    {
+        public string nome { get; private set; }
+        public double salario { get; private set; }
+
+        public Funcionario(string nome, double salario)
+        {
+            this.nome = nome;
+            this.salario = salario;
+        }
+
+        public void MediaSalario(Funcionario funcionario1, Funcionario funcionario2)
+        {
+            double mediaSalarial;
+
+            mediaSalarial = (funcionario1.salario + funcionario2.salario) / 2;
+
+            Console.WriteLine("Salário médio = "+mediaSalarial.ToString("F2"), CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ ExercicioQualMaiorSalario/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ExercicioQualMaiorSalario;
+using System.Globalization;
+
+Console.WriteLine("Dados do primeiro funcionário: ");
+Console.Write("Nome: ");
+string nome = Console.ReadLine();
+Console.Write("Salário: ");
+double salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+Funcionario funcionario1 = new Funcionario(nome,salario);
+Console.WriteLine("Dados do primeiro funcionário: ");
+Console.Write("Nome: ");
+string nome2 = Console.ReadLine();
+Console.Write("Salário: ");
+double salario2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+Funcionario funcionario2 = new Funcionario(nome2, salario2);
+
+funcionario1.MediaSalario(funcionario1,funcionario2);
+
